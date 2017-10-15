83e97350535859de021e7e7dccebe0ea52597528
Hsueh-Ting Chu <Hsueh-Ting Chu>
2017-10-16 02:13:26 +0800

    init

---
+++ ai2/MemCare_0820/src/appinventor/ai_htchu_taiwan/MemCare_0820/Screen3.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"159","Source":"Form","Properties":{"$Name":"Screen3","$Type":"Form","$Version":"20","AppName":"X2","Title":"Screen3","Uuid":"0","$Components":[{"$Name":"HorizontalArrangement1","$Type":"HorizontalArrangement","$Version":"3","AlignHorizontal":"3","Width":"-2","Uuid":"-223280873","$Components":[{"$Name":"Button1","$Type":"Button","$Version":"6","Text":"Text for Button1","Uuid":"-486114394"}]},{"$Name":"WebViewer1","$Type":"WebViewer","$Version":"6","Uuid":"-978644214"}]}}
+|#
